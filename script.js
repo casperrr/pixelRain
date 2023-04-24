2ddcf38b2672bf0b5a5d6020a9fadf6cceaf8aa5
casperrr <casper.sebastien@gmail.com>
2023-04-24 14:07:07 +0100

    working rain with image

--- script.js
+++ script.js
@@ -5,13 +5,40 @@ img.src = "res/thomYorke.jpg";
 img.addEventListener("load",()=>{
     const canvas = document.getElementById("canvas");
     const c = canvas.getContext("2d");
-    c.drawImage(img,0,0,canvas.width,canvas.height);
     canvas.width = 800;
     canvas.height = 800;
+    
+    c.drawImage(img,0,0,canvas.width,canvas.height);
+    const pixels = c.getImageData(0,0,canvas.width,canvas.height);
     
     let particlesArray = [];
     let partNum = 5000;
 
+
+    let mappedImage = [];
+    for (let y = 0; y < canvas.height; y++){
+        let row = [];
+        for (let x = 0; x < canvas.width; x++){
+            const red = pixels.data[(y*4*pixels.width)+(x*4)]
+            const green = pixels.data[(y*4*pixels.width)+(x*4+1)]
+            const blue = pixels.data[(y*4*pixels.width)+(x*4+2)]
+            const brightness = calculateRelativeBrightness(red,green,blue);
+            const cell = [
+                cellBrightness = brightness,
+            ];
+            row.push(cell);
+        }
+        mappedImage.push(row);
+    }
+
+    function calculateRelativeBrightness(r,g,b){
+        return Math.sqrt(
+            (r*r)*0.299 +
+            (g*g)*0.587 +
+            (b*b)*0.114
+        )/100;
+    }
+    
     class Particle {
         constructor(){
             this.x = Math.random() * canvas.width;
@@ -19,9 +46,17 @@ img.addEventListener("load",()=>{
             this.speed = 0;
             this.velocity = Math.random() * 0.5;
             this.size = Math.random() * 1.5 + 1;
+            this.pos1 = Math.floor(this.y);
+            this.pos2 = Math.floor(this.x);
         }
 
         update(){
+            this.pos1 = Math.floor(this.y);
+            this.pos2 = Math.floor(this.x);
+            this.speed = mappedImage[this.pos1][this.pos2][0];
+            let movement = (2.5-this.speed)+this.velocity;
+
+            this.y += movement;
             this.y += this.velocity;
             if(this.y >= canvas.height){
                 this.y = 0;
@@ -43,6 +78,7 @@ img.addEventListener("load",()=>{
     }
     init();
     function animate(){
+        //c.drawImage(img,0,0,canvas.width,canvas.height);
         c.globalAlpha = 0.05;
         c.fillStyle = "rgb(0,0,0)";
         c.fillRect(0,0,canvas.width,canvas.height);
