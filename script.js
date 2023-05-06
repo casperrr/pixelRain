fc3b55ccbf177ad01e13ff2ee7e9017d0fb41e72
casperrr <casper.sebastien@gmail.com>
2023-05-06 16:08:38 +0100

    angle changes on brightness spiral rain

--- script.js
+++ script.js
@@ -50,17 +50,22 @@ img.addEventListener("load",()=>{
             this.size = Math.random() * 1.5 + 1;
             this.pos1 = Math.floor(this.y);
             this.pos2 = Math.floor(this.x);
+            this.angle = 0;
         }
 
         update(){
             this.pos1 = Math.floor(this.y);
             this.pos2 = Math.floor(this.x);
-            this.speed = mappedImage[this.pos1][this.pos2][0];
+            if((mappedImage[this.pos1])&&(mappedImage[this.pos1][this.pos2])){
+                this.speed = mappedImage[this.pos1][this.pos2][0];
+            }
             let movement = (2.5-this.speed)+this.velocity;
+            this.angle+=this.speed/20;
 
-            this.y += movement;
+            this.y += movement + Math.sin(this.angle) * 2;
             this.y += this.velocity;
-            this.x += movement;
+            this.x += movement + Math.cos(this.angle) * 2;
+            //Out of bounds checker
             if(this.y >= canvas.height){
                 this.y = 0;
                 this.x = Math.random() * canvas.width;
@@ -73,7 +78,9 @@ img.addEventListener("load",()=>{
 
         draw(){
             c.beginPath();
-            c.fillStyle = mappedImage[this.pos1][this.pos2][1];
+            if((mappedImage[this.pos1])&&(mappedImage[this.pos1][this.pos2])){
+                c.fillStyle = mappedImage[this.pos1][this.pos2][1];
+            }
             c.arc(this.x,this.y,this.size,0,Math.PI * 2);
             c.fill();
         }
